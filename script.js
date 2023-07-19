7b83a32814433c9d3c8375d64784991f88725d04
kocpiepait <f.barahama01@gmail.com>
2023-07-19 09:43:49 +0700

    Tugas

---
+++ script.js
@@ -0,0 +1,12 @@
+// Toggle class active
+
+const navbar = document.querySelector
+
+('.navbar');
+// ketika menu diclick
+
+document.querySelector('#menu' ).onclick = () => {
+
+    navbar.classList.toggle('active');
+
+};
